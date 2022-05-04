2c0d5373d58b2f62e5e7cbedf4b85b9bbed08f77
umaparvathir <48677402+umaparvathir@users.noreply.github.com>
2022-05-05 00:06:22 +0530

    updated app1

--- app1/app1.py
+++ app1/app1.py
@@ -5,7 +5,7 @@ app1 = Flask(__name__)
 
 @app1.route('/')
 def hello_world():
-  return 'Salam alikom, this is App1 :) '
+  return 'Westwing, this is App1 :) '
 
 if __name__ == '__main__':
   app1.run(debug=True, host='0.0.0.0')
